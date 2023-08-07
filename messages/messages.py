9466bb94f024b6f25e012a29d204c1c67851aae1
Ayllionne <84089058+Ayllionn@users.noreply.github.com>
2023-08-07 13:42:01 +0200

    Update messages.py

--- messages/messages.py
+++ messages/messages.py
@@ -2,12 +2,12 @@ import os
 from glob import glob
 
 def get(txt_name):
-    files = glob(os.getcwd()+"\\messages\\*.txt")
+    files = glob(os.getcwd()+"/messages/*.txt")
 
     messages = {}
 
     for f in files:
         with open(f, 'r', encoding="utf8") as file:
-            messages.update({f.split("\\")[-1][:-4]:file.read()})
+            messages.update({f.split("\\")[-1].split("/")[-1][:-4]:file.read()})
 
     return messages.get(txt_name)

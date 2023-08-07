af09284211b158e8aaaff2d700b753e5cc6391b1
Ayllionne <84089058+Ayllionn@users.noreply.github.com>
2023-08-07 15:08:11 +0200

    Delete log directory

--- log/__init__.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-import os
-
-def log(i, cmd):
-    date = datetime.now()
-    day = f"{date.day}-{date.month}-{date.year}"
-    log_time = f'{date.hour}:{date.minute}:{date.second}'
-    with open(f"{os.getcwd()}/log/log/{day}.txt", "a+", encoding="utf8") as file:
-        file.write(f"{log_time}   :  {i.user.id} - {i.user} EXECUTED CMD : {cmd}\n")

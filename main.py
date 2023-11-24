79a4e00d27e4ca9addd32cb5ab837df959bfb057
Ayllionne <84089058+Ayllionn@users.noreply.github.com>
2023-11-25 00:28:43 +0100

    Update main.py
    je suis débile

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ import time
 from Bot import bot
 
 print("Lancement du bot\n_____________________________________________________________________________________________")
-quit()
 time.sleep(1)
 bot.load()

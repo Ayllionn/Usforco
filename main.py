92f355cb7fa9e332c2623763817c2362bd4719a3
Ayllionne <84089058+Ayllionn@users.noreply.github.com>
2023-12-29 18:19:24 +0100

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-import time
-from Bot import bot
-
-print("Lancement du bot\n_____________________________________________________________________________________________")
-time.sleep(1)
-bot.load()

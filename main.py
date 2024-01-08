17864b21fe4a3ae4448f127d09fd2a30c48da7f0
Ayllionne <84089058+Ayllionn@users.noreply.github.com>
2024-01-08 22:49:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import os
+
+try:
+    import discord
+except:
+    os.system("pip install discord")
+
 from Server import Serveur
 
 if __name__ == '__main__':
     srv = Serveur()
     srv.load()
-
